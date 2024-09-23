754191be14d6eae69a19f2f46e0189b1a631ef66
mahrarhussain07 <mahrarhussain9@gmail.com>
2024-09-23 23:39:14 +0500

    added playground thing of graphiql

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require("dotenv").config();
 var { createHandler } = require("graphql-http/lib/use/express");
+const expressPlayground =
+  require('graphql-playground-middleware-express').default;
 const schema = require("./schema/schema");
 const port = process.env.PORT || 5000;
 
@@ -10,9 +12,10 @@ app.all(
   "/graphql",
   createHandler({
     schema,
-    graphiql: process.env.NODE_ENV === "development",
   })
 );
+app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+
 app.listen(port, () => {
   console.log("listening on port ", port);
 });
