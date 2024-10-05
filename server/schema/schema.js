cd11049792cc0001cad650e97389e40e42a47870
mahrarhussain07 <mahrarhussain9@gmail.com>
2024-10-05 18:49:25 +0500

    updated schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,8 +3,19 @@ const {
   GraphQLID,
   GraphQLString,
   GraphQLSchema,
+  GraphQLList,
 } = require("graphql");
 const { projects, clients } = require("../sampleData");
+
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+});
 
 const ClientType = new GraphQLObjectType({
   name: "Client",
@@ -19,7 +30,26 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parents, args) {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parents, args) {
+        return projects.find((pro) => pro.id === args.id);
+      },
+    },
     clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parents, args) {
+        return clients;
+      },
+    },
+    client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
       resolve(parents, args) {
