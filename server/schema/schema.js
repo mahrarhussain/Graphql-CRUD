98e519b36d68776aa3443b9e0a1da2faf0b98220
mahrarhussain07 <mahrarhussain9@gmail.com>
2024-10-07 19:04:08 +0500

    added addProject in schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -98,6 +98,35 @@ const mutation = new GraphQLObjectType({
       },
     },
 
+    addProject: {
+      type: ProjectType,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        description: { type: new GraphQLNonNull(GraphQLString) },
+        status: {
+          type: new GraphQLEnumType({
+            name: "ProjectStatus",
+            values: {
+              new: { value: "Not Started" },
+              progress: { value: "In Progress" },
+              completed: { value: "Completed" },
+            },
+          }),
+          defaultValue: "Not Started",
+        },
+        clientId: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        const project = new Project({
+          name: args.name,
+          description: args.description,
+          status: args.status,
+          clientId: args.clientId,
+        });
+
+        return project.save();
+      },
+    },
   },
 });
 
